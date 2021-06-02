f05cbc3d004c4a6eba239cb39aa5108d2356bf65
gonior <arthemuss14@gmail.com>
2021-06-03 03:52:37 +0700

    Add Home Page

--- app/src/main/java/com/example/gniar/MainActivity.kt
+++ app/src/main/java/com/example/gniar/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.gniar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.gniar.menus.*
 
 import com.example.gniar.room.PersonalDB
+import kotlinx.android.synthetic.main.activity_main.*
 
 // tanggal pengerjaan   : Senin, 31 Mei 2021
 // nim                  : 10118901
@@ -12,9 +15,33 @@ import com.example.gniar.room.PersonalDB
 
 class MainActivity : AppCompatActivity() {
 
+    private val homeFragment = HomeFragment()
+    private val dailyFragment = DailyFragment()
+    private val galelryFragment = GalelryFragment()
+    private val favFragment = FavFragment()
+    private val profileFragment = ProfileFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(homeFragment)
 
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(homeFragment)
+                R.id.daily -> replaceFragment(dailyFragment)
+                R.id.gallery -> replaceFragment(galelryFragment)
+                R.id.fav -> replaceFragment(favFragment)
+                R.id.profile -> replaceFragment(profileFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        if(fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.flFragment, fragment)
+            transaction.commit()
+        }
     }
 }
